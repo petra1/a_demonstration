98c513535ec7fa57f89dade720be541dbcae6312
Petra Ritter <ritter1.apps@gmail.com>
2025-03-08 15:08:16 +0100

    Now an English-language HTML file is displayed

--- lib/pages/header.dart
+++ lib/pages/header.dart
@@ -1,14 +1,28 @@
-import 'package:a_demonstration/widgets/web_view.dart';
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
-class Header extends StatelessWidget {
+class Header extends StatefulWidget {
   const Header({super.key});
+
+  @override
+  State<Header> createState() => _HeaderState();
+}
+
+class _HeaderState extends State<Header> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController();
+    _controller.loadFlutterAsset('assets/html/heading.html');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: WebView(),
+      body: WebViewWidget(controller: _controller),
     );
   }
 }
