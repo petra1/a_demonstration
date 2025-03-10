3a3f6887d342ad5dd9ec8d2b3593d53ad29afa21
Petra Ritter <ritter1.apps@gmail.com>
2025-03-10 08:55:05 +0100

    k Displaying different HTML content should work now.

--- lib/pages/header.dart
+++ lib/pages/header.dart
@@ -1,3 +1,4 @@
+import 'dart:io'; // Importiere die benötigte Bibliothek
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -15,11 +16,30 @@ class _HeaderState extends State<Header> {
   void initState() {
     super.initState();
     _controller = WebViewController();
-    _controller.loadFlutterAsset('assets/html/heading.html');
   }
 
   @override
   Widget build(BuildContext context) {
+    // Prüfe die aktuelle Locale und die Plattform-Helligkeit
+    String localeName = Platform.localeName;
+    bool isDarkMode =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+
+    // Lade die entsprechende HTML-Datei basierend auf den Bedingungen
+    String assetToLoad;
+
+    if (localeName.startsWith('de') && isDarkMode) {
+      assetToLoad = 'assets/html/dark_mode_heading_de.html';
+    } else if (isDarkMode) {
+      assetToLoad = 'assets/html/dark_mode_heading.html';
+    } else if (localeName.startsWith('de')) {
+      assetToLoad = 'assets/html/heading_de.html';
+    } else {
+      assetToLoad = 'assets/html/heading.html';
+    }
+
+    // Lade die ausgewählte HTML-Datei
+    _controller.loadFlutterAsset(assetToLoad);
     return Scaffold(
       appBar: AppBar(),
       body: WebViewWidget(controller: _controller),
